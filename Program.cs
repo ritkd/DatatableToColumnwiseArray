e537e113e8327994e1e7b0517c53dfa03c2d35ce
ritkd <ritesh.msit@gmail.com>
2022-07-01 20:07:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DatatableToColumnwiseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create datatable and fill with dummydata
+            int Rows = 10; //1 million
+            int Cols = 10;
+            DataTable MockTable = new DataTable().GetDataTableWithMockDataExtension(Rows,Cols);
+            List<string[]> Result =MockTable.ToColumnwiseArray();
+             
+            foreach (string[] item in Result)
+            {
+                foreach (string val in item)
+                {
+                    Console.WriteLine(val,"/n");
+                }
+                Console.WriteLine("/n/n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatableToColumnwiseArray
+{
+    static class Utility
+    {
+        internal static DataTable GetDataTableWithMockDataExtension(this DataTable dt, int RowCOunt, int ColCount)
+        {
+            for (int i = 0; i < ColCount; i++)
+            {
+                dt.Columns.Add("col" + (i + 1).ToString());
+            }
+            for (int i = 0; i < RowCOunt; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["col1"] = "sdfdsfsfsef";
+                dr["col2"] = "rtytetytrytr";
+                dr["col3"] = "xcvxcvcvxvxcxcxc";
+                dr["col4"] = "asdasdasdasdadasdasd";
+                dr["col5"] = "qweqweweqweqwewewewwqeweqwe";
+                dr["col6"] = "pouioioyioiyoyuyuyuyuiuyijtyuti";
+                dr["col7"] = "fmbhvnbncngnbncnggngngncgngcccngcgcn";
+                dr["col8"] = "fxxcchchgvuvvvvjhbjvccchjhvvcfcchjhbbkbjh";
+                dr["col9"] = "osidufohofosdhvhrohvohohwdhvsdhvhohsdohcosdhco";
+                dr["col10"] = "lkljsdlklkjljvldslkjlkfjldskjldjlkdjlkcjlzxkjclkzxjl";
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+
+        internal static List<string[]> ToColumnwiseArray(this DataTable dt)
+        {
+            List<string[]> ColumnwiseArray = new List<string[]>();
+            foreach (DataColumn column in dt.Columns)
+            {
+                List<string> col = (from Products in dt.AsEnumerable()
+                                    select Products.Field<string>(column.ColumnName)).ToList();
+                string[] arr = col.ToArray();
+                ColumnwiseArray.Add(arr);
+            }
+            return ColumnwiseArray;
+        }    
+    }
+}
